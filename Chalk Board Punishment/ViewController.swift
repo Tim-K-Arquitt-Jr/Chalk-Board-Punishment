41c051dfbb5c94a13cb03eadf470cee4bd72564e
Timothy Arquitt <timothyarquitt@timothys-macbook-pro-2.local>
2019-04-11 14:59:48 -0500

    The start of the project.

--- Chalk Board Punishment/ViewController.swift
+++ Chalk Board Punishment/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
+var sliderCount = 0
+var userText: String = " "
+
+
+
 class ViewController: UIViewController {
 
+//    Outlets
+    @IBOutlet weak var textInputField: UITextField!
+    @IBOutlet weak var sliderNumberDisplay: UILabel!
+    @IBOutlet weak var wordDisplay: UILabel!
+   
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    
+//    actions
+    @IBAction func acceptButton(_ sender: Any) {
+    
+        userText = textInputField.text!
+        
+        wordDisplay.text = userText
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+@IBAction func sliderButtonPressed(_ sender: Any) {
+    
+    sliderNumberDisplay.text = String(Int(sliderNumberDisplay.value))
+    
+}
+    
+    
 }
 
